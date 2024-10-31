ad0376266c554e8102145ff8d8f10ef2182bcccc
jakubforman <me@jakubforman.eu>
2024-10-31 13:12:57 +0100

    Add numeric input functionality with basic calculator UI
    Implemented the ability to input and display numeric values using buttons. Added basic calculator layout and style, including number and operator buttons. Removed unused CSS associated with the old template.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Component} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonInput,
+  IonRow,
+  IonGrid, IonCol
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonRow, IonGrid, IonCol],
 })
 export class HomePage {
-  constructor() {}
+
+  input = "0";
+
+  constructor() {
+  }
+
+
+  addNum(number: number) {
+    if (this.input === "0") {
+      this.input = "";
+    }
+
+    this.input = this.input + number;
+  }
+
+  removeNum() {
+    console.log(this.input.length)
+    this.input = this.input.slice(0, this.input.length - 1);
+
+    if (this.input === "") {
+      this.input = "0";
+    }
+
+
+  }
 }
