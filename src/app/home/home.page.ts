9157c1d624846123047069ce5366f21d0ee22ac5
jakubforman <me@jakubforman.eu>
2024-11-07 14:20:56 +0100

    Add numeric input and basic calculator operations
    Implemented numeric input handling and added basic arithmetic operations including addition and toggle sign. Updated the UI buttons to support these functions, enhancing the calculator's usability.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,12 +19,28 @@ import {
 })
 export class HomePage {
 
+  /**
+   * HTML Input placeholder
+   *
+   * Hold values for HTML input
+   */
   input = "0";
+
+  private holdInput = "";
+
+  private action: '+' | '-' | '/' | '*' | null = null;
 
   constructor() {
   }
 
 
+  /**
+   * Adding numbers
+   *
+   * Add number to input field
+   *
+   * @param number Integer from 0 to 9
+   */
   addNum(number: number) {
     if (this.input === "0") {
       this.input = "";
@@ -33,6 +49,11 @@ export class HomePage {
     this.input = this.input + number;
   }
 
+  /**
+   * Remove number / AC
+   *
+   * Removing last character from input field
+   */
   removeNum() {
     console.log(this.input.length)
     this.input = this.input.slice(0, this.input.length - 1);
@@ -40,7 +61,51 @@ export class HomePage {
     if (this.input === "") {
       this.input = "0";
     }
+  }
 
+  /**
+   * Toggle sign
+   *
+   * Toggling +/- on input field
+   */
+  toggleSign() {
+    if (this.input === "0") {
+      return;
+    }
 
+    if (this.input[0] === "-") {
+      this.input = this.input.slice(1);
+    } else {
+      this.input = "-" + this.input;
+    }
+  }
+
+  adding() {
+    if (this.action) {
+      this.processAction();
+    }
+
+    console.log(this.input);
+
+    this.action = "+";
+    this.holdInput = this.input;
+    this.input = "0";
+  }
+
+  private processAction() {
+    switch (this.action) {
+      case "+":
+        this.input = (Number(this.input) + Number(this.holdInput)).toString();
+        break;
+      case "-":
+        this.input = (Number(this.input) - Number(this.holdInput)).toString();
+        break;
+      case "/":
+        this.input = (Number(this.input) / Number(this.holdInput)).toString();
+        break;
+      case "*":
+        this.input = (Number(this.input) * Number(this.holdInput)).toString();
+        break;
+    }
   }
 }
